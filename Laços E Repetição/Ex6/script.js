7dcab816aaa77c0a290062665bebb1e0d9d743b0
willianrizzo <willianrizzoratao10@gmail.com>
2023-06-04 13:31:11 -0300

    ex6 ok

---
+++ Laços E Repetição/Ex6/script.js
@@ -0,0 +1,15 @@
+let saida = "";
+
+for (let i = 1; i <= 20; i++) {
+    saida += i + "\n";
+}
+
+alert(saida);
+
+let saida2 = "";
+
+for (let i = 1; i <= 20; i++) {
+    saida2 += i + " ";
+}
+
+alert(saida2);
